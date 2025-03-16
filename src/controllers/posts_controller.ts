af479b1766fa84f52889a83db312dba8b97ad900
mayadahan77 <mayaschool555@gmail.com>
2025-03-16 23:06:36 +0200

    fix PR conflict

--- src/controllers/posts_controller.ts
+++ src/controllers/posts_controller.ts
@@ -19,10 +19,8 @@ class PostsController extends BaseController<IPost> {
   async create(req: Request, res: Response) {
     const userId = req.params.userId;
     const image = req.file ? base + req.file?.path : null;
-    const image = req.file ? base + req.file?.path : null;
     const post = {
       ...req.body,
-      imgUrl: image,
       imgUrl: image,
       senderId: userId,
     };
@@ -58,98 +56,6 @@ class PostsController extends BaseController<IPost> {
         }
       }
       res.status(201).send(createdPost);
-    } catch (error) {
-      res.status(400).send(error);
-    }
-  }
-
-  async getAll(req: Request, res: Response) {
-    const filter = req.query.sender;
-    const skip = parseInt(req.query.skip as string) || 0;
-    const limit = parseInt(req.query.limit as string) || 10;
-
-    try {
-      let items;
-      let totalItems;
-      if (filter) {
-        items = await this.model.find({ senderId: filter }).sort({ createdAt: -1 }).skip(skip).limit(limit);
-        totalItems = await this.model.countDocuments({ senderId: filter });
-      } else {
-        items = await this.model.find().sort({ createdAt: -1 }).skip(skip).limit(limit);
-        totalItems = await this.model.countDocuments();
-      }
-      const populatedItems = await Promise.all(
-        items.map(async (i) => {
-          const user = await userModel.findOne({ _id: i.senderId });
-          const commentsCount = await commentModel.countDocuments({ postId: i._id });
-          return {
-            ...i.toObject(),
-            senderName: user?.fullName,
-            senderProfile: user?.imgUrl,
-            commentsCount: commentsCount,
-          };
-        })
-      );
-      res.send({
-        totalItems,
-        items: populatedItems,
-      });
-    } catch (error) {
-      res.status(400).send(error);
-    }
-  }
-
-  async getById(req: Request, res: Response) {
-    const id = req.params.id;
-    try {
-      const item = await this.model.findById(id);
-      if (item != null) {
-        const user = await userModel.findOne({ _id: item.senderId });
-        const commentsCount = await commentModel.countDocuments({ postId: item._id });
-        const populatedItem = {
-          ...item.toObject(),
-          senderName: user?.fullName,
-          senderProfile: user?.imgUrl,
-          commentsCount: commentsCount,
-        };
-
-        res.send(populatedItem);
-      } else {
-        res.status(404).send("not found");
-      }
-    } catch (error) {
-      res.status(400).send(error);
-    }
-  }
-
-  async updateItem(req: Request, res: Response): Promise<void> {
-    const id = req.params.id;
-    let body = {};
-    if (req.file) {
-      body = {
-        ...req.body,
-        imgUrl: base + req.file?.path,
-      };
-    } else {
-      body = req.body;
-    }
-    try {
-      const rs = await this.model.findByIdAndUpdate(id, body, { new: true });
-      if (!rs) {
-        res.status(404).send();
-      } else {
-        const user = await userModel.findOne({ _id: rs.senderId });
-        const commentsCount = await commentModel.countDocuments({ postId: rs._id });
-        const populatedItem = {
-          ...rs.toObject(),
-          senderName: user?.fullName,
-          senderProfile: user?.imgUrl,
-          commentsCount: commentsCount,
-        };
-
-        res.send(populatedItem);
-      }
-      return;
     } catch (error) {
       res.status(400).send(error);
     }
